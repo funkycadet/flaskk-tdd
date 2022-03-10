e93fc513f0af166c9bbd17c117344419684e10dc
Eric Alaribe <ericsmithsonian2@gmail.com>
2022-03-10 04:44:28 +0100

    Added testing for database file

--- tests/app_test.py
+++ tests/app_test.py
@@ -1,4 +1,5 @@
-from urllib import response
+from pathlib import Path
+
 from project.app import app
 
 def test_index():
@@ -7,3 +8,6 @@ def test_index():
 
     assert response.status_code == 200
     assert response.data == b"Hello, World!"
+
+def test_database():
+    assert Path("flaskk.db").is_file()
